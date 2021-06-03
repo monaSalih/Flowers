2a5c38f41304ed0bf06c3c7777b99c464077b56c
monaalobisat <>
2021-06-03 12:49:00 +0000

    script

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-var name= prompt("inter your name");
+var name= prompt("enter your name");
+  document.write("<h1 style='color:#b83b5e;' >welcome "+name +"</h1> ");
+  document.write("<img src='https://images.unsplash.com/photo-1472214103451-9374bd1c798e?ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cGljfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80' width=130px hight=130px />");
 
-if (name != null){
-   alert("welcome "+name);
-}else {
- alert("please inter your name");
-}
-document.write("greate job");
+ var add= prompt("what is the result for 5+5");
+ if (add == 10 ){
+ alert ("coorect answer");
+     }else
+     {alert("you must study hard");     }
+
+
+// document.write("<img src='https://images.unsplash.com/photo-1472214103451-9374bd1c798e?ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cGljfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80'/>)";
+
+ 
+
+
+
+
+
